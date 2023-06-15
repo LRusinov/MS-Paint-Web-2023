ce0776ad7505c265a0b4ed6066adda25dd25808f
LRusinov <lubomir202020@gmail.com>
2023-06-15 14:18:18 +0300

    Switched to fabric.js library.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, Inject } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'project';
-  @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
-  context!: CanvasRenderingContext2D;
+  fabricCanvas!: fabric.Canvas;
   // Sets the default color
   currentColor: string = 'black';
   currentTool: string = 'pencil';
@@ -47,77 +46,42 @@ export class AppComponent {
   }
 
   //Sets the current tool.
+
   setTool(tool: string) {
     this.currentTool = tool;
+    if (tool === 'pencil') {
+      this.fabricCanvas.isDrawingMode = true;
+    } else {
+      this.fabricCanvas.isDrawingMode = false;
+      this.fabricCanvas.selection = true; // Add this line to enable object selection
+    }
   }
   ngOnInit() {
-    // Getting the context of Canvas for painting
-    this.context = this.canvas.nativeElement.getContext(
-      '2d'
-    ) as CanvasRenderingContext2D;
-    this.context.fillStyle = 'white';
-    this.context.fillRect(
-      0,
-      0,
-      this.canvas.nativeElement.width,
-      this.canvas.nativeElement.height
-    );
-    this.context.strokeStyle = this.currentColor;
-  }
-  ngAfterViewInit() {
-    //Mouse position variables
-    let lastX = 0;
-    let lastY = 0;
-
-    // Listening to the "mousedown" event on canvas
-    this.canvas.nativeElement.addEventListener('mousedown', (event) => {
-      // Remembering the start position of the maouse
-      lastX = event.offsetX;
-      lastY = event.offsetY;
-      // Starting the painting path
-      this.context.beginPath();
-      this.context.lineWidth = 5;
-      this.context.moveTo(lastX, lastY);
+    this.fabricCanvas = new fabric.Canvas('canvas', {
+      preserveObjectStacking: true,
     });
+    this.fabricCanvas.freeDrawingBrush.width = 5;
+    this.fabricCanvas.freeDrawingBrush.color = this.currentColor;
+    this.fabricCanvas.backgroundColor = 'white';
 
-    // Listening to the event "mousemove" on canvas
-    this.canvas.nativeElement.addEventListener('mousemove', (event) => {
-      // Check if the mouse button is pressed
-      if (event.buttons !== 1) return;
-
-      // Drawing a line from the previous position to the current position
-      this.context.lineTo(event.offsetX, event.offsetY);
-      this.context.stroke();
-    });
+    this.fabricCanvas.renderAll();
   }
 
   // Sets the color for writing
   setColor(color: string) {
-    this.context.strokeStyle = color;
     this.currentColor = color;
+    this.fabricCanvas.freeDrawingBrush.color = color;
   }
 
   // Clears the canvas
   clear() {
-    this.context.clearRect(
-      0,
-      0,
-      this.canvas.nativeElement.width,
-      this.canvas.nativeElement.height
-    );
-    this.context.fillStyle = 'white';
-    this.context.fillRect(
-      0,
-      0,
-      this.canvas.nativeElement.width,
-      this.canvas.nativeElement.height
-    );
-    this.context.strokeStyle = this.currentColor;
+    this.fabricCanvas.clear();
+    this.fabricCanvas.backgroundColor = 'white';
   }
 
   downloadCanvas() {
     let canvas = <HTMLCanvasElement>document.getElementById('canvas');
-    let canvasUrl = canvas.toDataURL('image/jpeg', 0.5);
+    let canvasUrl = canvas.toDataURL('image/png', 0.5);
 
     console.log(canvasUrl);
     const createEl = document.createElement('a');
