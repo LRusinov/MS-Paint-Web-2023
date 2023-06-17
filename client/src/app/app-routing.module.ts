9a5fc74b8be92372e960930c34a5bfa500e51556
LRusinov <lubomir202020@gmail.com>
2023-06-17 23:39:07 +0300

    Removed old components.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,23 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PaintingPageComponent } from './components/painting-page/painting-page.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { FileSaverComponent } from './components/file-saver/file-saver.component';
 
-const routes: Routes = [
-  {
-    path: 'paint',
-    component: PaintingPageComponent,
-  },
-  {
-    path: 'toolbar',
-    component: ToolbarComponent,
-  },
-  {
-    path: 'file-saver',
-    component: FileSaverComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PaintingPageComponent } from './components/painting-page/painting-page.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { FileSaverComponent } from './components/file-saver/file-saver.component';
 
 @NgModule({
-  declarations: [AppComponent, PaintingPageComponent, ToolbarComponent, FileSaverComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
